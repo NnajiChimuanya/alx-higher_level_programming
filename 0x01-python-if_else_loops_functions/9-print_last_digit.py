b46112f15041202566fcf683c68d72c292c44a2e
Nnaji Chimuanya <nnajichimuanya@gmail.com>
2023-03-07 20:06:23 -0800

    rewrote the algo for better optimization of memmory

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    last = abs(number) % 10
-    print(f'{last}')
+    last = 0
+    
+    if number > 0:
+        last = number % 10
+        print(f'{last}')
+    else:
+        last = number % -10
+        print(f'{last}')
     return last
